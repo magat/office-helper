2a5b100db223699182f1710d5ca208445c37ca9c
Gaëtan Le Moing <3ck0o@users.noreply.github.com>
2016-02-18 15:45:44 +0100

    Added basic view to add and view lists of requests Imported also Thymeleaf layout dialect

--- service/src/main/java/com/officehelper/service/RequestService.java
+++ service/src/main/java/com/officehelper/service/RequestService.java
@@ -2,7 +2,6 @@ package com.officehelper.service;
 
 import com.officehelper.dao.RequestDAO;
 import com.officehelper.dto.RequestDTO;
-import com.officehelper.entity.Author;
 import com.officehelper.entity.Request;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,9 +30,8 @@ public class RequestService {
 
     @Transactional
     public long addRequest(RequestDTO req) {
-        //TODO : TEMPORARY, TO REPLACE FUTURE SESSION SYSTEM
         Request r = req.toRequest();
-        r.setAuthor(new Author());
+        r.setAuthor(req.getAuthor().toAuthor());
         return requestDAO.addRequest(r);
     }
 
